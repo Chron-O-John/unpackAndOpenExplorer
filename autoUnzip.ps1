48fecfee7a3ce2b8c905153ca94bd57bec3af7cb
Chron-O-John <36130691+Chron-O-John@users.noreply.github.com>
2024-03-10 12:09:36 +0100

    Replaced PowerShell's ExpandArchive with 7zip
    ExpandArchive is too slow

--- autoUnzip.ps1
+++ autoUnzip.ps1
@@ -22,8 +22,13 @@ if (-not (Test-Path -Path $zipFilePath)) {
 $destinationPath = (Join-Path -Path (Split-Path -Path $zipFilePath) -ChildPath (Split-Path -Path $zipFilePath -Leaf).Replace('.zip', ''))
 
 # Unzip the file
-Expand-Archive -Path $zipFilePath -DestinationPath $destinationPath
+$arguments = "x `"$zipFilePath`" -o`"$destinationPath\`""
 
+# Replaced with 7zip, as Expand-Archive is too slow
+# Expand-Archive -Path $zipFilePath -DestinationPath $destinationPath
+Start-Process -NoNewWindow -Wait -FilePath "C:\Program Files\7-Zip\7z.exe" -ArgumentList $arguments
+
+pause
 # Delete the zip file
 Remove-Item -Path $zipFilePath -Force
 
@@ -31,13 +36,11 @@ Remove-Item -Path $zipFilePath -Force
 $explorer = Start-Process -FilePath "explorer.exe" -ArgumentList "/root\,$destinationPath" -PassThru
 Start-Sleep -Milliseconds 500
 $foldername = Split-Path $destinationPath -Leaf
-#echo $explorer
+
     (New-Object -ComObject 'Shell.Application').Windows() | ForEach-Object { 
         if ($_.locationName -contains "$foldername") {
              $pwnd = $_.HWND
         }
     }
-
-
-#$hwnd = (Get-Process -Id $explorer.Id).MainWindowHandle
+	
 [Program]::SetForegroundWindow($pwnd)
